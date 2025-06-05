1bd69a18aa5b74171865e648291eed37ff711fd2
batzul <lbatzul170@gmail.com>
2025-06-05 11:19:00 +0900

    Add project

---
+++ ConsoleApp5.29/ConsoleApp5.29/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5._29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> 
+            { "Satoshi", "Yuki", "Hana", "Ken", "Haruka", "Yuka" };
+
+            List<string> result = names
+                .Where(n => n.Length >= 4)
+                .OrderBy(n => n)
+                .Select(n => n.ToUpper())
+                .ToList();
+
+            foreach(string name in result)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
+
